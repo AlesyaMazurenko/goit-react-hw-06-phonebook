d480fd0eb765fa2a38fbcdf9841e03c1b5e0e30f
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-12-03 17:38:01 +0100

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -5,13 +5,14 @@ export default function ContactList({ items, removeContact }) {
     console.log(items);
     const elements = items.map(({ name, id, number }) => {
         return <li className="contacts_item" key={id}>
+            
             {name}: {number}
-            <button type="button" className='btn_remove' onClick={() => removeContact(id)}>Remove</button>
+            <button type="button" className='btn_remove' onClick={() => removeContact(id)}>X</button>
         </li>
     })
 
     return (
-        <ul className="contact=list">{elements}</ul>
+        <ul className="contact_list">{elements}</ul>
     )
 }
 
